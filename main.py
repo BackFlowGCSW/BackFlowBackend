5979adf8328a8e31774dcf7ca82b674249624b54
mangoesafterplay <ja2021069823@virtual.upt.pe>
2025-05-11 22:10:33 -0500

    [@mangoesafterplay] Nueva route para Solicitudes de cambio

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routes.usuario_routes import router as usuarios_router
 from routes.organizacion_routes import router as organizaciones_router
 from routes.proyecto_routes import router as proyectos_router
 from routes.tarea_routes import router as tareas_router
+from routes.solicitud_routes import router as solicitudes_cambio_router
 import config.db
 
 app = FastAPI()
@@ -11,6 +12,7 @@ app.include_router(usuarios_router)
 app.include_router(organizaciones_router)
 app.include_router(proyectos_router)
 app.include_router(tareas_router)
+app.include_router(solicitudes_cambio_router)
 
 @app.get("/")
 def root():
